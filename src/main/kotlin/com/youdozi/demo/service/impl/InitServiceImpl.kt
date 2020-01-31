3cf41b37960aaf235b52593fbf0700c8329de3e6
youdozi <eschoi@systemk.kr>
2020-02-01 01:50:11 +0900

    Transactinal import 변경

--- src/main/kotlin/com/youdozi/demo/service/impl/InitServiceImpl.kt
+++ src/main/kotlin/com/youdozi/demo/service/impl/InitServiceImpl.kt
@@ -5,8 +5,8 @@ import com.youdozi.demo.repository.ArticleRepository
 import com.youdozi.demo.service.InitService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import javax.annotation.PostConstruct
-import javax.transaction.Transactional
 
 @Service
 class InitServiceImpl : InitService {
