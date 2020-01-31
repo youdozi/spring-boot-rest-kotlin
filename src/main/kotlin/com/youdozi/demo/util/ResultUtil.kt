6e79b1b8a3e353b0a8d560fba2eeeba216d7693a
youdozi <eschoi@systemk.kr>
2020-01-31 16:46:20 +0900

    ResultUtil 추가

--- src/main/kotlin/com/youdozi/demo/util/ResultUtil.kt
+++ src/main/kotlin/com/youdozi/demo/util/ResultUtil.kt
@@ -1,4 +1,45 @@
-package com.youdozi.demo
+package com.youdozi.demo.util
 
-class ResultUtil {
+import java.util.*
+
+object ResultUtil {
+
+    // Common Result
+    fun setCommonResult(resultCode: String, resultMessage: String): Map<String, Any> {
+        val obj: MutableMap<String, Any> = LinkedHashMap()
+        obj["resultCode"] = resultCode
+        obj["resultMessage"] = resultMessage
+        return obj
+    }
+
+    // Common Object Result
+    fun setCommonResult(resultCode: String, resultMessage: String, tempObj: Any): Map<String, Any> {
+        val obj: MutableMap<String, Any> = LinkedHashMap()
+        val detailObj: MutableMap<String, Any> = LinkedHashMap()
+        obj["resultCode"] = resultCode
+        obj["resultMessage"] = resultMessage
+        detailObj["content"] = tempObj
+        obj["data"] = detailObj
+        return obj
+    }
+
+    // Common List Result
+    fun setCommonResult(resultCode: String, resultMessage: String, tempList: List<Any?>): Map<String, Any> {
+        val obj: MutableMap<String, Any> = LinkedHashMap()
+        val detailObj: MutableMap<String, Any> = LinkedHashMap()
+        obj["resultCode"] = resultCode
+        obj["resultMessage"] = resultMessage
+        detailObj["content"] = tempList
+        obj["data"] = detailObj
+        return obj
+    }
+
+    // Common Result Error
+    fun setCommonResult(resultCode: String, resultMessage: String, errorMessage: String): Map<String, Any> {
+        val obj: MutableMap<String, Any> = LinkedHashMap()
+        obj["resultCode"] = resultCode
+        obj["resultMessage"] = resultMessage
+        obj["errorMessage"] = errorMessage
+        return obj
+    }
 }
