4dce6162ebae7ae0d1d259647c58b8d46e573386
youdozi <eschoi@systemk.kr>
2020-02-10 23:57:18 +0900

    Jlb 추가(Docker Build)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.3.61"
 	kotlin("plugin.spring") version "1.3.61"
 	kotlin("plugin.jpa") version "1.3.61"
+	id("com.google.cloud.tools.jib") version "1.0.0"
 }
 
 group = "com.youdozi"
